cf07fba694c701e8f2b439d98b5aae7fbdd4de86
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2023-08-27 00:53:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function upload(){
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("img")
+    var data = new Date()
+    var hours = data.getHours()
+    msg.innerHTML = `Now, it is ${hours} hours.`
+    hours = 23
+    if(hours >= 8 && hours < 12){
+        //Good Morning
+        img.src = 'morning.png'
+        document.body.style.background = "#ff7f00"
+    } else if(hours >= 12 && hours < 18){
+        //Good Afternoon
+        img.src = 'afternoon.png'
+        document.body.style.background = "#f1bc7b"
+    } else{
+        //Good Night
+        img.src = 'night.png'
+        document.body.style.background = "#001f3a"
+    }
+}
